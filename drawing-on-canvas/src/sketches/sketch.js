9669f8a120d82545bcd85b05f116f953dd7c1962
Bruno Bortagaray <bruno.bortagaray@highsnobiety.com>
2022-11-02 14:39:33 +0100

    feat: Interaction with keyboard. simple sh*t

--- drawing-on-canvas/src/sketches/sketch.js
+++ drawing-on-canvas/src/sketches/sketch.js
@@ -1,22 +1,46 @@
 // p = p5 Sketch Object
 export default function Sketch(p) {
+  let posX = 0,
+    posY = 0;
+
+  p.keyPressed = () => {
+    switch (p.keyCode) {
+      case p.LEFT_ARROW:
+        posX -= 10;
+        break;
+      case p.RIGHT_ARROW:
+        posX += 10;
+        break;
+      case p.UP_ARROW:
+        posY -= 10;
+        break;
+      case p.DOWN_ARROW:
+        posY += 10;
+        break;
+      default:
+        break;
+    }
+  };
   p.preload = () => {};
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+
     p.rectMode(p.CENTER);
-    p.strokeWeight(10);
-    p.background(0);
   };
 
   p.draw = () => {
-    p.translate(p.width / 2, p.height / 2);
-    p.fill(255, 0, 0);
-    p.rect(0, 0, 500, 500);
+    p.background(0);
 
-    p.push();
-    p.fill(0);
-    p.ellipse(0, 0, 100);
-    p.pop();
+    // map literally maps a number to another
+    //In this case, I'm mapping the values of the mouse
+    //axis to a number between 0 (when close to the edge of the screen)
+    // and 500 (when close to the other edge of the screen)
+    const mapX = p.map(p.mouseX, 0, p.width, 0, 500);
+    const mapY = p.map(p.mouseY, 0, p.height, 0, 500);
+
+    p.translate(p.width / 2, p.height / 2);
+    // p.rect(posX, posY, 500, 500);
+    p.rect(posX, posY, 500, 500);
   };
 }
